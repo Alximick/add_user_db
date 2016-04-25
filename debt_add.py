a0da133c23e772b957d202e7b1c8af41b1f68849
Alximick <alximick@gmail.com>
2016-04-25 12:47:39 +0600

    edit script

--- debt_add.py
+++ debt_add.py
@@ -5,27 +5,44 @@ import os
 from itertools import count
 
 import re
-import xlrd
+import xlrd, xlwt
 import django
 
+CONFIG = {'ком.': 'community', 'охрана': 'guard',
+          'э/энергия': 'power', 'снег': 'snow',}
+
+
 def parser_debt_type(filename):
-    lots = {}
+    # lots = parser_lot(filename)
     read_book = xlrd.open_workbook(filename, on_demand=True,
                                    encoding_override="utf-8")
     sheet = read_book.sheet_by_index(0)
-    mounths = ('Янв', 'Фев', 'Мар', 'Апр', 'Ма', 'Июн','Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек' , '15')
-    debt_type = ('Эл/Эн', 'Штраф', '15')
-    for row in sheet.row_values(0):
-        if type(row) == str and len(row) > 0:
-            for mounth in mounths:
-                if re.search(mounth.lower(), row.lower()):
-                    for debt_t in debt_type:
-                        if re.search(debt_t.lower(), row.lower()):
-                            print('mounth', mounth, 'за что ', debt_t, ' = ', row)
-        # if type(row) == float:
-        #     print('float', row)
+    dct = {}
+    first_row = sheet.row_values(0)
+    for index in range(len(first_row)):
+        if first_row[index] in CONFIG:
+            dct[index] = CONFIG[first_row[index]]
+    for rownum in range(sheet.nrows):
+        row = sheet.row_values(rownum)
+        for index in range(len(row)):
+            if index in dct and row[index] and row[0] \
+                    and type(row[index]) == float:
+                print(int(row[0]), first_row[index],
+                      '(',dct[index], ') = ',row[index])
 
-    return  lots
+    # mounths = ('Янв', 'Фев', 'Мар', 'Апр', 'Ма', 'Июн','Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек' , '15')
+    # debt_type = ('Эл/Эн', 'Штраф', '15')
+    # for row in sheet.row_values(0):
+    #     if type(row) == str and len(row) > 0:
+    #         for mounth in mounths:
+    #             if re.search(mounth.lower(), row.lower()):
+    #                 for debt_t in debt_type:
+    #                     if re.search(debt_t.lower(), row.lower()):
+    #                         print('mounth', mounth, 'за что ', debt_t, ' = ', row)
+    #     # if type(row) == float:
+    #     #     print('float', row)
+
+    # return  lots
 
 def add_debt_type(debt_type):
     '''Заготовок'''
