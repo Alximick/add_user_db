ef2b6759a529601a07f1e54132e014c3563d1bce
alexey <alximick@gmail.com>
2016-04-26 19:22:39 +0600

    edit parser

--- debt_add.py
+++ debt_add.py
@@ -2,194 +2,64 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-
 import re
 import xlrd
 import django
 
 
-CONFIG = {'ком.': 'community', 'охрана': 'guard',
+DEBT_TYPE_DCT = {'ком.': 'community', 'охрана': 'guard',
           'э/энергия': 'power1', 'снег': 'snow',
           'Эл/Эн': 'power', 'Штраф': 'fine',
-          'None': 'None'
-          }
+          'None': 'None'}
+
 
 DEBT_TYPE = ('Эл/Эн', 'Штраф', 'ком.', 'охрана', 'э/энергия', 'снег')
-MOUNTHS = ('Янв', 'Фев', 'Мар', 'Апр', 'Ма', 'Июн',
-           'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек')
-DCT_MOUNTHS = {'Янв': 1, 'Фев': 2, 'Мар': 3, 'Апр': 4, 'Ма': 5, 'Июн': 6,
-               'Июл': 7, 'Авг': 8, 'Сен': 9, 'Окт': 10, 'Ноя': 11, 'Дек': 12}
 
 
-def create_debt_type(name_type, slug_type):
-
-    from debt.models import DebtType, Debt
-    from loginsys.admin import MyUser
-    # print(name_type, slug_type)
-    obj, created = DebtType.objects.get_or_create(name=name_type, slug=slug_type)
-    # print(obj, created)
+MONTH_ALL = ('Янв', 'Фев', 'Мар', 'Апр', 'Ма', 'Июн',
+         'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек')
 
 
-def create_debt(user, debt_type, years, month, amount, print_value=False):
-    username = None
-    from debt.models import DebtType, Debt
-    from loginsys.admin import MyUser
-    if type(user) == float:
-        username = MyUser.objects.get(username=int(user))
-    elif type(user) == str:
-        username = MyUser.objects.get(username=user)
-    else:
-        import ipdb; ipdb.set_trace()
-    obj, created = Debt.objects.get_or_create(
-        type=DebtType.objects.get(slug=debt_type),
-        year=years,
-        user=username,
-        month=month,
-        amount=amount,
-    )
-    if print_value:
-        print(obj, created)
+DCT_MONTH = {'Янв': 1, 'Фев': 2, 'Мар': 3, 'Апр': 4, 'Ма': 5,
+             'Июн': 6, 'Июл': 7, 'Авг': 8, 'Сен': 9, 'Окт': 10,
+             'Ноя': 11, 'Дек': 12, 'None': None}
 
 
 def parser_debt_type(filename):
-    read_book = xlrd.open_workbook(filename, on_demand=True,
-                                   encoding_override="utf-8")
+
+    read_book = xlrd.open_workbook(filename, on_demand=True)
     sheet = read_book.sheet_by_index(0)
-    dct = {}
-    YEAR = None
     first_row = sheet.row_values(0)
-    if not first_row[0]:
-        print('Введите год в первую ячейку')
-        print('На пример 2014')
-        exit(1)
-    try:
-        YEAR = int(first_row[0])
-    except:
-        print('некорректно введен год')
-        print('Введите год в первую ячейку')
-        print('На пример 2014')
-        exit(1)
-    second_row = sheet.row_values(1)
-    sum = 0
-    # if row[index] = second_row[0]:
-    #     sum += row[index]
-    print(YEAR)
-    for rownum in range(sheet.nrows):
-        if rownum == 0:
-            continue
-        row = sheet.row_values(rownum)
 
-        for index in range(len(first_row)):
-            flag = True
-            if type(first_row[index]) == str and len(first_row[index]) > 0 and row[0] and row[index]:
-                for mounth in MOUNTHS:
-                    if re.search(mounth.lower(), first_row[index].lower()):
-                        for debt_t in DEBT_TYPE:
-                            if re.search(debt_t.lower(), first_row[index].lower()):
-                                dct[index] = debt_t
-                                create_debt_type(debt_t, CONFIG[debt_t])
-                                create_debt(row[0], CONFIG[debt_t], YEAR, DCT_MOUNTHS[mounth],
-                                            row[index])
-                                flag = False
-                                if row[0] == second_row[0]:
-                                    print(row[0], row[index], sum)
-                                    sum += row[index]
-                                    create_debt(row[0], CONFIG[debt_t], YEAR,
-                                                DCT_MOUNTHS[mounth],
-                                                row[index], True)
-                                # print(row[0], debt_t, 'Год', DCT_MOUNTHS[mounth], row[index])
-                if row[0] and row[index] and flag:
-                    for debt_t in DEBT_TYPE:
-                        if re.search(debt_t.lower(), first_row[index].lower()):
-                            # print(row[0], debt_t, 'Год', 'месяц', row[index])
-                            dct[index] = debt_t
-                            create_debt_type(debt_t, CONFIG[debt_t])
-                            if row[0] == second_row[0]:
-                                print(row[0], row[index], sum)
-                                sum += row[index]
-                                create_debt(row[0], CONFIG[debt_t], YEAR, None,
-                                            row[index], True)
-                            create_debt(row[0], CONFIG[debt_t], YEAR, None, row[index])
-            elif type(first_row[index]) == float and index > 0 and row[0] and row[index]:
-                year, month, *tail = xlrd.xldate_as_tuple(first_row[index], 0)
-                dct[index] = 'None'
-                # print(row[0], 'None', year, month, row[index])
-                create_debt_type('None', 'None')
-                if row[0] == second_row[0]:
-                    print(row[0], row[index], sum)
-                    sum += row[index]
-                    create_debt(row[0], 'None', year, month, row[index], True)
-                create_debt(row[0], 'None', year, month, row[index])
-    # print(len(first_row))
-    sum_second_row = 0
-    for sec_row in second_row[1:]:
-        if sec_row:
-            sum_second_row += sec_row
-    if first_row[-1] == 'Итого':
-        print(second_row[-1], sum)
+    print(filename)
+    year = re.search(r'\d+', filename.lower()).group()
+    lst = [filename,]
 
-    else:
-        print(sum, sum_second_row)
+    for index in range(len(first_row)):
+        if type(first_row[index]) == str:
+            month_column = 'None'
+            for month in MONTH_ALL:
+                if re.search(month.lower(), first_row[index].lower()):
+                    month_column = month
+            for debt_t in DEBT_TYPE:
+                if re.search(debt_t.lower(), first_row[index].lower()):
+                    # print(CONFIG[debt_t], year, DCT_MONTH[m])
+                    index, type_debt, year, month = index, DEBT_TYPE_DCT[debt_t], year, DCT_MONTH[month_column]
+                    lst.append((index, type_debt, year, month))
+                    break
+        elif type(first_row[index]) == float and first_row[index] > 2:
+            year, month, *tail = xlrd.xldate_as_tuple(first_row[index], 0)
+            # print('ком.', year, month)
+            index, type_debt, year, month = index, DEBT_TYPE_DCT['ком.'], year, month
+            lst.append((index, type_debt, year, month))
 
-    print(dct)
-
+    print(lst)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('usage: ./create_users.py file')
-        sys.exit(1)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
     django.setup()
-    debt_type = parser_debt_type(sys.argv[1])
-
-
-# def test():
-#     print(int(first_row[0]))
-#     years = re.match(r'\d+', first_row[0])
-#
-#     for i in first_row[1:]:
-#         if i == 'Итого' or i == '':
-#             continue
-#         if type(i) == str:
-#             print(i)
-#         if type(i) == float:
-#             year, month, *tail = xlrd.xldate_as_tuple(i, 0)
-#             print('None', year, month)
-#
-#     # Create DebtType
-#     for index in range(len(first_row)):
-#         if first_row[index] in CONFIG:
-#             dct[index] = CONFIG[first_row[index]]
-#     obj, created = DebtType.objects.get_or_create(name=first_row[index],
-#                                                   slug=CONFIG[first_row[index]])
-#
-#     print(obj, created)
-#
-#
-#     # Create Debt
-#     for rownum in range(sheet.nrows):
-#         row = sheet.row_values(rownum)
-#         for index in range(len(row)):
-#             if index in dct and row[index] and row[0] and type(row[index]) == float:
-#                 # username = None
-#                 if type(row[0]) == float:
-#                     username = MyUser.objects.get(username=int(row[0]))
-#                 elif type(row[0]) == str:
-#                     username = MyUser.objects.get(username=row[0])
-#                 else:
-#                     import ipdb; ipdb.set_trace()
-#                 obj, created = Debt.objects.get_or_create(
-#                     type=DebtType.objects.get(slug=CONFIG[first_row[index]]),
-#                     year=int(years.group()),
-#                     user=username,
-#                     month=None,
-#                     amount=row[index],
-#                 )
-#                 print(obj, created)
-#
-#     for rownum in range(sheet.nrows):
-#         row = sheet.row_values(rownum)
-#         for index in range(len(row)):
-#             if row[index]:
-#                 print(row[index], end='')
-#         print()
+    # debt_type = parser_debt_type(sys.argv[1])
+    files = os.listdir(os.getcwd())
+    my_xlsx = filter(lambda x: x.endswith('.xlsx'), files)
+    for file in my_xlsx:
+        parser_debt_type(file)
