d49211ccd13734f0510cea77038e974f88c8165f
Alximick <alximick@gmail.com>
2016-05-04 05:12:36 +0600

    three requests to the database

--- debt/views.py
+++ debt/views.py
@@ -27,13 +27,13 @@ def my_all(debt, debt_type, years, sum):
 
     for d_type in debt_type:
         row = []
-        row.append(d_type.name)
-        not_found = True
+        row.append(d_type)
         for year in years:
+            not_found = True
             for item in debt:
-                if item['year'] == year and item['type__name'] == d_type.name:
-                    url_sum_year = r'<a href="' + url.url + str(year) + r'">' +  \
-                                   str(item['sum_year']) + r'</a>'
+                if item['year'] == year and item['type__name'] == d_type:
+                    url_sum_year = r'<a href="' + url.url + str(year) + r'">' \
+                                   + str(item['sum_year']) + r'</a>'
                     row.append(url_sum_year)
                     not_found = False
             if not_found:
@@ -47,27 +47,30 @@ def my_all(debt, debt_type, years, sum):
 
 def my_all_filter(debt, debt_type, sum):
     all_row = []
-    row = ['']
+
+    row = []
     for item in debt:
         year, month = item.year, item.month
-        row.append((year, month))
-    all_row.append(set(row))
+        row.append((month, year))
+    row = sorted(set(row))
+    row.insert(0, '')
+    all_row.append(row)
 
     for d_type in debt_type:
         row = []
-        print(d_type)
         row.append(d_type)
         not_found = True
         for year_month in all_row[0]:
             if not year_month:
                 continue
             for item in debt:
-                if item.year == year_month[0] and item.month == year_month[1]\
-                        and item.type.name == d_type.name:
+                if item.year == year_month[1]\
+                        and item.month == year_month[0]\
+                        and item.type.name == d_type:
                     row.append(item.amount)
                     not_found = False
             if not_found:
-                row.append('')
+                row.append('<td></td>')
         all_row.append(row)
 
     all_row.append(row_sum(sum, len(all_row[0])))
@@ -79,31 +82,23 @@ def mydebt(request, year=None):
     args = {}
 
     if year:
-        debt = Debt.objects.filter(user_id=request.user.id, year=year).select_related('type')
-        args['filter'] = True
-        args['debt'] = debt
+        args['debt'] = Debt.objects.filter(user_id=request.user.id, year=year)\
+            .select_related('type')
+        args['debt_type'] = {item.type.name for item in args['debt']}
+        args['sum'] = sum([item.amount for item in args['debt']])
+        args['years'] = sorted({item.year for item in args['debt']})
+        args['all'] = my_all_filter(args['debt'], args['debt_type'], args['sum'])
     else:
-        debt = Debt.objects.filter(user_id=request.user.id).select_related('type')
-        args['filter'] = False
-        args['debt'] = debt.values('year', 'type__name').annotate(sum_year=Sum('amount'))
-
-    debt_type = {item.type for item in debt}
-    args['debt_type'] = debt_type
-    args['sum'] = sum([item.amount for item in debt])
-    args['years'] = sorted({item.year for item in debt})
-
-
-    if not args['filter']:
-
+        args['debt'] = Debt.objects.filter(user_id=request.user.id)\
+            .select_related('type').values('year', 'type__name')\
+            .annotate(sum_year=Sum('amount'))
+        args['years'] = sorted({item['year'] for item in args['debt']})
+        args['debt_type'] = {item['type__name'] for item in args['debt']}
+        args['sum'] = sum([item['sum_year'] for item in args['debt']])
+        args['all'] = my_all(args['debt'], args['debt_type'], args['years'], args['sum'])
         if len(args['years']) == 1:
             return_url = redirect('mydebt').url + str(args['years'][0])
-            # print(return_url)
             return redirect(return_url)
-
-        args['all'] = my_all(args['debt'], args['debt_type'], args['years'], args['sum'])
-
-    if args['filter']:
-        args['all'] = my_all_filter(args['debt'], args['debt_type'], args['sum'])
 
 
     return render(request, 'jinja2/jinja2_view_debt.html', args)
