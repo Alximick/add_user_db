c147e42f92d783043f9a3ad01a89591a4b3aa48d
Alximick <alximick@gmail.com>
2016-05-03 18:24:45 +0600

    refactoring

--- debt/views.py
+++ debt/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from debt.models import Debt
-from django.contrib import auth
 from django.db.models import Sum, Count
 
 
@@ -9,14 +8,15 @@ def mydebt(request, year=None):
     if year:
         debt = Debt.objects.filter(user_id=request.user.id, year=year).select_related('type')
         args['filter'] = True
+        args['debt'] = debt
     else:
         debt = Debt.objects.filter(user_id=request.user.id).select_related('type')
         args['filter'] = False
-    args['debt'] = debt
-    args['debt_type'] = {item.type for item in debt}
+        args['debt'] = debt.values('year', 'type__name').annotate(sum_year=Sum('amount'))
 
-    args['sum'] = debt.aggregate(Sum('amount'))
+    args['debt_type'] = {item.type for item in debt}
+    args['sum'] = sum([item.amount for item in debt])#debt.aggregate(Sum('amount'))
     args['years'] = {item.year for item in debt}
-    args['test'] = debt.values('year', 'type__name').annotate(Sum('amount'))
+    
     return render(request, 'jinja2/jinja2_view_debt.html', args)
 
